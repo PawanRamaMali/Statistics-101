f34d072a8f7aa628daa6f54ba7ab372321a8fe7e
Pawan Rama Mali <prm@outlook.in>
2021-04-06 22:34:58 +0530

    ver 0.1

--- Simple Linear Regression/app/ui.R
+++ Simple Linear Regression/app/ui.R
@@ -1,12 +1,6 @@
 ###################
 # ui.R
-# 
-# Initializes the ui. 
-# Used to load in your header, sidebar, and body components.
-###################
 
-# If shinydashboard library is not imported here, the dashboard does not work 
-# on Shiny Server.
 
 library(shinydashboard)
 
